8943e156c06a6c6308cfc8be002d413108ee2d3b
Marvin-Silva <dejesusdasilva.marvin>
2025-07-06 23:30:24 +0200

    :wrench: configuring swagger docs

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 import express from 'express';
 import router from './routes/landing_page.js';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 
 /*
     This is the web server of the application
@@ -9,6 +11,7 @@ import cors from 'cors';
 const app = express();
 const port = 3000;
 
+// Accept all external requests
 app.use(cors());
 
 // Middleware
@@ -18,6 +21,29 @@ app.use(express.json());
 // Routes
 app.use('/api/v1', router);
 
+// Swagger configuration
+const swaggerOptions  = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Carambar API Node.js Express',
+            version:'1.0.0',
+            description: 'Documentation auto-générée avec Swageer'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Serveur lancé sur http://localhost:${port}`);
+    console.log(`Swagger dispo sur http://localhost:${port}/api-docs`);
 });
