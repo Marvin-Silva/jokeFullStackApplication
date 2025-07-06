9a9ea2bd29b9d3a28181c086e36862ca02ff288e
Marvin-Silva <dejesusdasilva.marvin>
2025-07-06 21:47:48 +0200

    :necktie: Updating dynamic file to listen click event

--- landing_page.js
+++ landing_page.js
@@ -1,18 +1,20 @@
-fetch('http://localhost:3000/api/v1/blagues')
+document.getElementById('btn-joke-random').addEventListener('click', function(){
+  fetch('http://localhost:3000/api/v1/blagues/random')
   .then(response => response.json())
-  .then(jokes => {
+  .then(joke => {
     const container = document.getElementById('jokes-container');
-    jokes.forEach(joke => {
-      const jokeDiv = document.createElement('div');
-      jokeDiv.style.marginBottom = "20px";
+  
+      const jokeDiv = document.getElementById('jokes-container-div');
 
       jokeDiv.innerHTML = `
-        <h3>${joke.question}</h3>
-        <p>${joke.response}</p>
+        <h3 id="joke-question">${joke.question}</h3>
+        <p id="joke-response">${joke.response}</p>
       `;
       container.appendChild(jokeDiv);
-    });
+    
   })
   .catch(err => {
     console.error('Erreur fetch:', err);
   });
+
+});
